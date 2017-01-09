aef717bd7b49f973e9f1c2fc8e72f040c26e05f5
mndowne <mndowne@gmail.com>
2017-01-08 22:44:30 -0600

    I missed a comma in app.get function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port',(process.env.PORT || 3000));
 app.use(express.static(__dirname));
 
-app.get('/' function(req,res){
+app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
